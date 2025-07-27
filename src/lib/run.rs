caa553a6923198e84ef013796ca77070c12094da
cvtsh248 <cvtsh248@gmail.com>
2025-07-27 17:09:53 +0800

    Refactoring

--- src/lib/run.rs
+++ src/lib/run.rs
@@ -1,11 +1,12 @@
 use super::eval;
 use super::environment;
 use super::parser;
+use std::rc::Rc;
+use std::cell::RefCell;
 
-// pub fn run_script(source: String) -> eval::RuntimeVal{
-//     let ast = parser::generate_ast(source);
-//     let mut environments_array: Vec<&mut environment::Environment> = vec![];
-//     let mut environment = environment::Environment {parent: None, variables: vec![]};
-//     let evaluate = eval::eval_program(&ast, &mut environment);
-//     evaluate
-// }
+pub fn run_script(source: String) -> eval::RuntimeVal{
+    let ast = parser::generate_ast(source);
+    let mut environment = Rc::new(RefCell::new(environment::Environment {parent: None, variables: vec![]}));
+    let evaluate = eval::eval_program(&ast, environment);
+    evaluate
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,19 @@
-use crate::lib::{environment, eval::RuntimeValType};
-use std::io::{self, Write};
-use std::rc::Rc;
-use std::cell::RefCell;
 mod lib;
-fn main() {
-    let source: String = 
-    "
-    let i = 1;
-    if (i < 10 && (false == true)){
-        i = 10;
-    }
-    i;
-    " .to_string();
-    // let tokens = lib::lexer::tokenise(source);
-    let ast = lib::parser::generate_ast(source);
-    println!("{:?}", ast);
-    let mut environment = Rc::new(RefCell::new(lib::environment::Environment {parent: None, variables: vec![]})); // lib::environment::Variable{ name: "x".to_string(), value: lib::eval::RuntimeVal{runtime_val_type:RuntimeValType::NumericInteger(2)} }
-    let evaluate = lib::eval::eval_program(&ast, environment);
-    println!("{:?}", evaluate);
-    // println!("{:?}", environment);
+use std::env;
+use std::fs;
 
-    // loop {
-    //     print!("> ");
-    //     io::stdout().flush().unwrap();
-    //     let mut input = String::new();
-    //     if io::stdin().read_line(&mut input).is_err() {
-    //         println!("Failed to read input.");
-    //         continue;
-    //     }
-    //     let input = input.trim();
-    //     if input == "exit" || input == "quit" {
-    //         break;
-    //     }
-    //     let ast = lib::parser::generate_ast(input.to_string());
-    //     // println!("{:?}", ast);
-    //     let result = lib::eval::eval_program(&ast, &mut environment);
-    //     println!("{:?}", result);
-    // }
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}",args);
+
+    let mut source: Option<String> = None;
+
+    if args.len() == 2 {
+        let file_path = &args[1];
+        source = Some(fs::read_to_string(file_path).unwrap()); 
+    }
+    
+    let out = lib::run::run_script(source.unwrap());
+    println!("{:?}", out);
 }
